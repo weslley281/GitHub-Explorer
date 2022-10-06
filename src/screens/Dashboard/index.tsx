779f8c9a35b947b37f2abba4479a1ca856a40990
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-06 06:57:36 -0400

    challenge resolved

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -16,14 +16,14 @@ import {
   InputField,
   InputButton,
   Icon,
-  RepositoriesList
+  RepositoriesList,
 } from './styles';
 
 type RootStackParamList = {
   Dashboard: undefined;
   Repository: {
     repositoryId: number;
-  }
+  };
 };
 
 type NavigationProps = StackNavigationProp<RootStackParamList, 'Dashboard'>;
@@ -37,22 +37,17 @@ export function Dashboard() {
   const { addRepository, repositories } = useRepositories();
 
   function handleAddRepository() {
-    /**
-     * TODO: 
-     * - call addRepository function sending inputText value;
-     * - clean inputText value.
-     */
+    if (inputText) {
+      addRepository(inputText);
+      setInputText('');
+      inputRef.current?.blur();
+    }
   }
 
   function handleRepositoryPageNavigation(id: number) {
-    /**
-     * TODO - navigate to the Repository screen sending repository id.
-     * Remember to use the correct prop name (repositoryId) to the repositoy id:
-     * 
-     * navigate(SCREEN NAME, {
-     *  repositoryId: id of the repository
-     * })
-     */
+    navigate('Repository', {
+      repositoryId: id,
+    });
   }
 
   return (
@@ -66,25 +61,17 @@ export function Dashboard() {
               ref={inputRef}
               placeholder="Digite aqui 'usuário/repositório'"
               value={inputText}
-              /**
-               * TODO - update inputText value when input text value 
-               * changes:
-               * onChangeText={YOUR CODE HERE}
-               */
+              onChangeText={setInputText}
               onSubmitEditing={handleAddRepository}
               returnKeyType="send"
-              autoCapitalize='none'
+              autoCapitalize="none"
               autoCorrect={false}
             />
 
             <InputButton
               testID="input-button"
               onPress={handleAddRepository}
-              /**
-               * TODO - ensure to disable button when inputText is 
-               * empty (use disabled prop to this):
-               * disabled={CONDITION HERE}
-               */
+              disabled={inputText == ''}
             >
               <Icon name="search" size={20} />
             </InputButton>
@@ -94,7 +81,7 @@ export function Dashboard() {
         <RepositoriesList
           data={repositories}
           showsVerticalScrollIndicator={false}
-          keyExtractor={repository => String(repository.id)}
+          keyExtractor={(repository) => String(repository.id)}
           renderItem={({ item: repository }) => (
             <Card
               key={repository.id}
@@ -102,7 +89,7 @@ export function Dashboard() {
                 id: repository.id,
                 title: repository.full_name,
                 subTitle: repository.description,
-                imageUrl: repository.owner.avatar_url
+                imageUrl: repository.owner.avatar_url,
               }}
               onPress={() => handleRepositoryPageNavigation(repository.id)}
             />
@@ -110,5 +97,5 @@ export function Dashboard() {
         />
       </Container>
     </Background>
-  )
+  );
 }
